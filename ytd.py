5a4723c6bc0a7720a747539653d0548533c5c5a8
Mazz <kmazzone93@gmail.com>
2023-02-10 17:57:58 -0500

    YT-Downloader V1.1 Fixed Progress Bar TODO: Add option to download Audio only(mp3). Make GUI(?) - (This would be in a later version)

--- ytd.py
+++ ytd.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 from sys import argv
 from tqdm import tqdm
 """
@@ -23,8 +24,7 @@ mp3(Audio Only - For music), or mp4(Whole Video) file."""
 """
 Version 1.0: Able to download mp4 to specified path.
 TODO:
-Add option to download Audio only(mp3).
-Fix progress bar so it updates in realtime. 
+Add option to download Audio only(mp3). 
 """
 
 # Path for where videos/mp3 files will be downloaded. Specify your own.
@@ -32,23 +32,25 @@ path = "C:/Users/kmazz/Desktop/YTvideos/"
 
 
 def vid_download(url):
-    yt = YouTube(url)
+    yt = YouTube(url, on_progress_callback=on_progress)
     fs = yt.streams.get_highest_resolution().filesize
     print(f"Link: {url}\n"
           f"Title: {yt.title}\n"
           f"Author: {yt.author}\n"
           f"Views: {yt.views}\n"
-          f"Length: {yt.length / 60} Min(s)\n"
+          f"Length: {yt.length // 60} Min(s)\n"
           f"FileSize: {fs / 1000000}")
     cmd = input("Press [ENTER] to Download or 'q' to quit: ")
-    if cmd == 'q':
+    if cmd == 'q' or cmd == 'quit':
         quit()
     else:
         done = False
         while not done:
             yd = yt.streams.get_highest_resolution()
+            yt.register_on_progress_callback(on_progress)
             progress = tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(fs), colour='green')
-            progress.update(int(fs))
+            for i in range(fs):
+                progress.update()
             yd.download(path)
             print(f"FINISHED!\nDownloaded: {yt.title}\nSaved in: '{path}\nFilesize: {fs / 1000000}mb")
             done = True
