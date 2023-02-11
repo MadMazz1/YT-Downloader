18ebda6fcca5c19467396150e706a4a3999e5d80
Mazz <kmazzone93@gmail.com>
2023-02-10 22:56:32 -0500

    YT-Downloader V1.2 Fixed Progress Bar Added option to download mp4 (Audio Only). This significantly reduces the size of the download and is optimal for downloading music files.

--- ytd.py
+++ ytd.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 from pytube.cli import on_progress
 from sys import argv
 from tqdm import tqdm
+import threading
 """
 Copyright (C) 2023  MazZ
 
@@ -20,7 +21,7 @@ Copyright (C) 2023  MazZ
 """
 """YT-Downloader(ytd.py) will download YouTube videos with a provided link. You may provide the link as an argument or the 
 program will ask for the user to input a link upon running. The user then chooses whether to download the file as an 
-mp3(Audio Only - For music), or mp4(Whole Video) file."""
+mp4(Audio Only - For music), or mp4(Whole Video) file."""
 """
 Version 1.0: Able to download mp4 to specified path.
 TODO:
@@ -39,7 +40,7 @@ def vid_download(url):
           f"Author: {yt.author}\n"
           f"Views: {yt.views}\n"
           f"Length: {yt.length // 60} Min(s)\n"
-          f"FileSize: {fs / 1000000}")
+          f"FileSize: {fs // 1000000}MB")
     cmd = input("Press [ENTER] to Download or 'q' to quit: ")
     if cmd == 'q' or cmd == 'quit':
         quit()
@@ -52,19 +53,80 @@ def vid_download(url):
             for i in range(fs):
                 progress.update()
             yd.download(path)
-            print(f"FINISHED!\nDownloaded: {yt.title}\nSaved in: '{path}\nFilesize: {fs / 1000000}mb")
+            print(f"FINISHED!\n"
+                  f"Downloaded: {yt.title}\n"
+                  f"Author: {yt.author}\n"
+                  f"Saved in: '{path}\n"
+                  f"Filesize: {fs // 1000000}MB")
+            done = True
+
+
+def music_download(url):
+    yt = YouTube(url)
+    music = yt.streams.get_audio_only("mp4")
+    fs = music.filesize
+    print(f"Link: {url}\n"
+          f"Title: {yt.title}\n"
+          f"Author: {yt.author}\n"
+          f"Views: {yt.views}\n"
+          f"Length: {yt.length // 60} Min(s)\n"
+          f"FileSize: {fs // 1000000}MB")
+    cmd = input(f"Press [ENTER] to Continue... or 'q' to quit.")
+    if cmd == 'q' or cmd == 'quit':
+        quit()
+    else:
+        done = False
+        while not done:
+            progress = tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(fs), colour='green')
+            for i in range(fs):
+                progress.update()
+            music.download(path)
+            print(f"FINISHED!\n"
+                  f"Downloaded: {yt.title}\n"
+                  f"Author: {yt.author}\n"
+                  f"Saved in: '{path}\n"
+                  f"Filesize: {fs // 1000000}MB")
             done = True
 
 
 def main():
-    while True:
+    done = False
+    while not done:
         if len(argv) > 5:
-            vid_download(argv[1])
+            choice = input(f"Would you like to download:\n"
+                           f"[1] Whole Video.(Highest Resolution)\n"
+                           f"[2] Just Audio. (Music)\n"
+                           f"Choose: ")
+            try:
+                if choice == '1':
+                    vid_download(argv[1])
+                    done = True
+                elif choice == '2':
+                    music_download(argv[1])
+                    done = True
+
+            except Exception as e:
+                raise print(f"That is not an option....")
+
         else:
             url = input(f"Please Enter a YouTube Link: ")
-            vid_download(url)
-        return False
+            choice = input(f"Would you like to download:\n"
+                           f"[1] Whole Video.\n"
+                           f"[2] Just Audio. (Music)\n"
+                           f"Choose: ")
+            try:
+                if choice == '1':
+                    vid_download(url)
+                    done = True
+                elif choice == '2':
+                    music_download(url)
+                    done = True
+
+            except Exception as e:
+                raise print(f"That is not an option....")
 
 
 if __name__ == '__main__':
-    main()
+    # Tried threading to download file and update progress bar at same time.
+    T1 = threading.Thread(target=main)
+    T1.start()
